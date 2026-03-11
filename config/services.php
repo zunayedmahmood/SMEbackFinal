65cbedbf26e365eff781f06f61b039760f35fe16
zunayedmahmood <zunayedmahmoodtahsin@example.com>
2026-03-11 16:52:28 +0600

    fixed imagekit env declaration

--- config/services.php
+++ config/services.php
@@ -41,4 +41,10 @@ return [
         'webhook_secret' => env('STRIPE_WEBHOOK_SECRET'),
     ],
 
+    'imagekit' => [
+        'public_key' => env('IMAGEKIT_PUBLIC_KEY'),
+        'private_key' => env('IMAGEKIT_PRIVATE_KEY'),
+        'url_endpoint' => env('IMAGEKIT_URL_ENDPOINT'),
+    ]
+
 ];
